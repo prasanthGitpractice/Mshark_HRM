82d40908661520743f60e2785d58c3466a45dfeb
prasanthGitpractice <galiprasanth6@gmail.com>
2023-11-28 16:19:35 +0530

    conflict test

---
+++ src/test/java/com/Project_Dummy/Practice/ClassConflict.java
@@ -0,0 +1,12 @@
+package com.Project_Dummy.Practice;
+
+import org.testng.annotations.Test;
+
+public class ClassConflict 
+{
+	@Test
+	public void conflict()
+	{
+		System.out.println("Conflict");
+	}
+}
