f93a6454569e565c3b06b5034b3b25b9c8e5b896
prasanthGitpractice <galiprasanth6@gmail.com>
2023-11-28 15:36:58 +0530

    independent commit

---
+++ src/test/java/com/Project_Dummy/Practice/ClassTest.java
@@ -0,0 +1,12 @@
+package com.Project_Dummy.Practice;
+
+import org.testng.annotations.Test;
+
+public class ClassTest 
+{
+	@Test
+	public void method()
+	{
+		System.out.println("Independent method");
+	}
+}
